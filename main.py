3bfd6472fa0de7ed409f88d400a9c39608a3118d
duskygloom <sarkarharsh708@gmail.com>
2023-09-21 16:22:55 +0530

    also using environment variable

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import asyncio, secret, logging
+import asyncio, logging, os
 from bot import create_bot
+
+try:
+    from secret import bot_token
+except ModuleNotFoundError:
+    bot_token = os.getenv("DISCORD_TOKEN")
 
 if __name__ == "__main__":
     try:
         bot = asyncio.run(create_bot())
-        bot.run(secret.bot_token)
+        bot.run(bot_token)
     except Exception as e:
         logging.error(e)
