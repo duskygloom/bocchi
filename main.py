bb8a7c47d1b725c670ff3b1234107ab7a90ba2d1
duskygloom <sarkarharsh708@gmail.com>
2023-09-22 00:53:56 +0530

    opus name is libopus

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio, logging, os, discord
 from bot import create_bot
 
-discord.opus.load_opus()
+discord.opus.load_opus("libopus")
 if not discord.opus.is_loaded():
     logging.warning("opus not loaded.")
 
