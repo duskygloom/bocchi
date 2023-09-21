fbaf85893e55f11f1eba87ff76c80b51263970bc
duskygloom <sarkarharsh708@gmail.com>
2023-09-21 23:28:39 +0530

    using ctypes to locate libopus

--- cogs/general.py
+++ cogs/general.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
-import discord
+import discord, ctypes
+
+ctypes.util.find_library("libopus")
 
 class General(commands.Cog):
     def __init__(self, bot: commands.Bot):
