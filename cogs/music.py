2d1482160ca8f2dc02da0ae762e2d9c10ffedbd6
duskygloom <sarkarharsh708@gmail.com>
2023-09-22 19:11:29 +0530

    music is usable (barely)

--- cogs/music.py
+++ cogs/music.py
@@ -1,4 +1,4 @@
-import discord, yt_dlp, logging
+import discord, logging
 from discord.ext import commands
 from yt_dlp import YoutubeDL
 
@@ -9,9 +9,10 @@ class Music(commands.Cog):
         self.voice_channel = None
         self.song_queue: dict[str, str] = []
         self.ytdl_options = { "noplaylist" : True }
+        self.ffmpeg_options = { "before_options": "-reconnect 2 -reconnect_streamed 2 -reconnect_delay_max 5" }
 
-    def add_song(self, query: str):
-        song = { "title": "", "url": "" }
+    def search_song(self, query: str) -> dict[str, str]:
+        song = {"title": "", "url": ""}
         try:
             # assuming query is url
             video = YoutubeDL(params=self.ytdl_options).extract_info(query, download=False)
@@ -20,34 +21,46 @@ class Music(commands.Cog):
             # assuming it's a normal search query
             try:
                 videos = YoutubeDL(params=self.ytdl_options).extract_info(f"ytsearch:{query}", download=False)
+                if len(videos["entries"]) == 0:
+                    return {}
                 info = videos["entries"][0]
             except Exception as e:
                 # something else happened
                 logging.error(e)
+                return {}
         song["title"] = info["title"]
         max_acodec = ""
         for format in info["formats"]:
-            keys = format.keys()
-            if "acodec" in keys and "vcodec" in keys and "ext" in keys and format["vcodec"] == "none" and format["ext"] == "m4a":
+            if "acodec" in format and "vcodec" in format and "ext" in format and format["vcodec"] == "none" and format["ext"] == "m4a":
                 if format["acodec"] > max_acodec:
                     max_acodec = format["acodec"]
                     song["url"] = format["url"]
-        self.song_queue.append(song)
+        return song
+
+    def add_song(self, query: str) -> bool:
+        song = self.search_song(query)
+        if song:
+            self.song_queue.append(song)
+            return True
+        else:
+            return False
 
     @commands.command(
         name="play",
         brief="Okay let me play this song."
     )
-    async def play(self, ctx: commands.Context):
+    async def play(self, ctx: commands.Context, *, song: str = None):
         if ctx.author.voice is None:
             await ctx.reply("You are not in any voice channel.", mention_author=False)
             return
         elif ctx.voice_client is None:
             self.voice_channel = await ctx.author.voice.channel.connect()
-        if len(self.song_queue) == 0:
+        if song is not None: song_url = self.search_song(song)["url"]
+        else: song_url = self.song_queue[0]['url']
+        if len(self.song_queue) == 0 and song is None:
             await ctx.reply("No songs in the queue.", mention_author=False)
             return
-        self.voice_channel.play(discord.FFmpegPCMAudio(self.song_queue[0]['url']))
+        self.voice_channel.play(discord.FFmpegPCMAudio(song_url, before_options=self.ffmpeg_options, options='-vn'))
         await ctx.message.add_reaction('✅')
 
     @commands.command(
@@ -63,10 +76,11 @@ class Music(commands.Cog):
             self.song_queue.pop(0)
             return
         self.song_queue.pop(0)
+        if self.voice_channel.is_playing():
+            self.voice_channel.pause()
         self.voice_channel.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(self.song_queue[0]['url'])))
         await ctx.reply(f"Playing {self.song_queue[0]['title']}", mention_author=False)
         
-
     @commands.command(
             name="stop",
             brief="Okay I will stop that song."
