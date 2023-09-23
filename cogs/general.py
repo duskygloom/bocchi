938995e0bde6a4c7785dc051f1a1251b2e09d117
duskygloom <sarkarharsh708@gmail.com>
2023-09-23 13:44:36 +0530

    started fixing genral

--- cogs/general.py
+++ cogs/general.py
@@ -1,59 +1,74 @@
 from discord.ext import commands
 from gtts import gTTS
 from utils.general import padded_intstring
+from utils.music import get_voice_client
 import discord, os
 
 class General(commands.Cog):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
-        self.bocchi_vc: discord.VoiceClient = None
-        self.song_queue = []
+        self.voice_client: discord.VoiceClient = None
 
     @commands.command(
-        name="greet",
-        brief="Hi user!",
-        description="Bocchi greets you in the voice chat.",
-        usage="!greet",
-        help="Bocchi greets you in the voice chat."
+            name="greet",
+            brief="Bocchi says hi."
     )
     async def greet(self, ctx: commands.Context):
-        if ctx.author.voice is None:
-            await ctx.reply("You are not in any voice channel.", mention_author=False)
-            return
-        if ctx.voice_client is None or self.bocchi_vc is None:
-            self.bocchi_vc = await ctx.author.voice.channel.connect()
+        self.voice_client = await get_voice_client(ctx, self.voice_client)
         greet_audio = "audio/hello.m4a"
-        self.bocchi_vc.play(discord.FFmpegPCMAudio(greet_audio))
+        if not os.path.isfile(greet_audio):
+            await self.speak(ctx)
+        self.voice_client.play(discord.FFmpegPCMAudio(greet_audio))
 
-    def is_bocchi_speaking(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
-        print(member, before, after)
+    @commands.command(
+            name = "speak",
+            brief = "Random Bocchi lines from the anime."
+    )
+    async def speak(self, ctx: commands.Context):
+        self.voice_client = await get_voice_client(ctx, self.voice_client)
+        clip_dir = "clips"
+        clip_ext = ".m4a"
+        clip_files = []
+        for file in clip_dir:
+            filepath = os.path.join(clip_dir, file)
+            if os.path.isfile(filepath) and filepath.endswith(clip_ext):
+                clip_files.append(filepath)
     
     @commands.command(
-        name="say",
-        brief="I say what you send.",
-        description="Bocchi repeats after you in the voice chat.",
-        usage="!say [what to say]",
-        help="Bocchi repeats after you in the voice chat."
+        name="google",
+        brief="Google translate like tts."
     )
-    async def say(self, ctx: commands.Context, *, text: str = "hi"):
-        if ctx.author.voice is None:
-            await ctx.reply("You are not in any voice channel.", mention_author=False)
-            return
-        elif ctx.voice_client is not None:
-            self.bocchi_vc = ctx.voice_client.channel
-        self.bocchi_vc = await ctx.author.voice.channel.connect()
+    async def google(self, ctx: commands.Context, *, text: str = "hi"):
+        self.voice_client = await get_voice_client(ctx, self.voice_client)
         tts_location = os.path.join("audio", "gtts")
         tts_file = "bocchi_tts_"
         index = 0
-        while os.path.isfile(os.path.join(tts_location, f"{tts_file}{self.padded_intstring(index)}.wav")):
+        while os.path.isfile(os.path.join(tts_location, f"{tts_file}{padded_intstring(index)}.wav")):
             index += 1
         tts_file = os.path.join(tts_location, f"{tts_file}{padded_intstring(index)}.wav")
         if not os.path.isdir(tts_location):
             os.mkdir(tts_location)
         text = text.lower()
-        tts_location = os.path.join(tts_location, text+'.wav')
         sound = gTTS(text)
         sound.save(tts_file)
-        self.bocchi_vc.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(tts_file)))
+        self.voice_client.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(tts_file)))
         os.remove(tts_file)
+
+    @commands.command(
+        name = "come",
+        brief = "Bocchi comes into your voice chat."
+    )
+    async def come(self, ctx: commands.Context):
+        self.voice_client = await get_voice_client(ctx, self.voice_client)
+        await ctx.message.add_reaction('✅')
+
+    @commands.command(
+        name = "go",
+        brief = "Bocchi goes out of your voice chat."
+    )
+    async def go(self, ctx: commands.Context):
+        if ctx.voice_client:
+            await ctx.voice_client.disconnect()
+        self.voice_client = None
+        await ctx.message.add_reaction('✅')
